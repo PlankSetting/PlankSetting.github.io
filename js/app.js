c84ebf80052d7c20621a51f2d2d7cf86bc87e2a6
Karim Oulad Chalha <herr.linux88@gmail.com>
2014-08-05 23:12:25 +0100

    fix arabic content1

--- js/app.js
+++ js/app.js
@@ -5,8 +5,4 @@
 	$('[data-spy="scroll"]').each(function () {
 	  var $spy = $(this).scrollspy('refresh')
 	});
-	Transifex.live.onTranslatePage(function('ar') {
-	  pic = "bottom-left";
-	  $('body').css('direction', 'rtl');
-	});
 })(jQuery);
